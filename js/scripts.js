871fe58a0cfb3de2a03ff2f5064e327c79067521
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-21 12:31:24 -0700

    if else works, need to refresh it.

--- js/scripts.js
+++ js/scripts.js
@@ -17,17 +17,16 @@ function PizzaConstructor(toppings, size) {
 
 PizzaConstructor.prototype.priceCheck = function () {
   var combinedSizeToppings = this.toppings + this.size;
-  if (combinedSizeToppings >= 2 && <=4) {
+  if (combinedSizeToppings >= 2 && combinedSizeToppings <= 4) {
     $('#output').text('3.99 what a deal')
   } else if (combinedSizeToppings === 5 ) {
     $('#output').text('4.99 wowzers')
   } else if (combinedSizeToppings === 6) {
     $('#output').text('5,000 dollars')
   }
-  console.log(combinedSizeToppings)
 }
 //user logic
-//creates new pizza object and ccaptures it in the var
+//creates new pizza object and captures it in the var
 var pizzaContainer = new Pizza();
 $(document).ready(function () {
   $("form#new-form").submit(function (event) {
@@ -35,7 +34,8 @@ $(document).ready(function () {
     //this captures toppngs/size
     var pizzaToppings = parseInt($("#toppings").val());
     var pizzaSize = parseInt($("#size").val());
-
+    $("#toppings").val("");
+    $("#size").val("");
     //this shows inputs to dom
     $("#show-input").text(pizzaToppings);
     $("#show-input").text(pizzaSize);
@@ -43,11 +43,11 @@ $(document).ready(function () {
     var order = new PizzaConstructor(pizzaToppings, pizzaSize)
     //this adds a pizza from the constructor
     pizzaContainer.addOrder(order)
-    var getPrice = pizzaContainer.za[o].priceCheck()
+    var getPrice = pizzaContainer.za[0].priceCheck()
+    console.log(getPrice)
     console.log(pizzaSize)
     console.log(pizzaToppings)
-    console.log(pizzaContainer.toppings)
-    console.log(pizzaContainer.size)
+
 
   });
 });
