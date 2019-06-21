ab4f99f2c14a169a49406d77d174409647a17013
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-21 08:57:38 -0700

    create pizza constructor for toppings and size.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,38 @@
+//business logic
+var toppings = [];
+var size = [];
+
+function Pizza() {
+  this.za = [];
+}
+
+Pizza.prototype.addPizza = function (input) {
+  this.za.push(input);
+}
+
+function PizzaConstructor(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
+
+
+
+
+
+
+
+
+
+
+//user logic
+var pizzaContainer = new Pizza();
 $(document).ready(function () {
   $("form#new-form").submit(function (event) {
     event.preventDefault();
-    var inputVariable = $("#new-input").val();
-    console.log(inputVariable);
-    $("#show-input").text(inputVariable);
+    var pizzaToppings = $("#toppings").val();
+    var pizzaSize = $("#size").val();
+    console.log(pizzaToppings,pizzaSize);
+    $("#show-input").text(pizzaFromInput);
+    var order = new PizzaConstructor(pizzaContainer)
   });
 });
