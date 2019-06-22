9ff5e866aad88be1c553c15e767ffe3a152078f0
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-22 11:46:09 -0700

    work on back end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,38 @@
 //Business logic for NewPie -----
 function NewPie() {
   this.pizzas = [];
+  this.currentId = 0;
 };
 
 NewPie.prototype.addPizza = function (pizza) {
+  pizza.id = this.assignId();
   this.pizzas.push(pizza);
+};
+
+NewPie.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+NewPie.prototype.findPizza = function (id) {
+  for (var i = 0; i < this.pizzas.length; i++) {
+    if (this.contacts[i]) {
+      if (this.pizzas[i].id == id) {
+        return this.pizzas[i];
+      }
+    }
+  };
+  return false;
+};
+
+NewPie.prototype.deletePizza = function (id) {
+  for (var i = 0; i < this.pizzas.length; i++) {
+    if (this.pizzas[i].id == id) {
+      delete this.contacts[i];
+      return true;
+    }
+  };
+  return false;
 };
 
 //Business logic for Pizzas--------
